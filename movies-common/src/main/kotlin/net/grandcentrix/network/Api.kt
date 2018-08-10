cb006e9a932cd67d7c59593d946466601a71a49a
StefMa <StefMaDev@outlook.com>
2018-08-10 11:38:51 +0200

    Get single Movie from API

--- movies-common/src/main/kotlin/net/grandcentrix/network/Api.kt
+++ movies-common/src/main/kotlin/net/grandcentrix/network/Api.kt
@@ -2,24 +2,31 @@ package net.grandcentrix.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.http.URLProtocol
-import kotlinx.coroutines.experimental.CoroutineDispatcher
 import kotlinx.coroutines.experimental.launch
+import kotlinx.serialization.json.JSON
+import net.grandcentrix.network.model.Movie
+
+private const val BASE_API = "api.themoviedb.org/3"
 
 class Api {
 
     private val client = HttpClient()
 
-    fun about(callback: (String) -> Unit) {
+    fun singleMovie(movieId: String, callback: (Movie) -> Unit) {
         launch {
             val result: String = client.get {
                 url {
-                    protocol = URLProtocol.HTTP
-                    host = "www.google.de"
+                    protocol = URLProtocol.HTTPS
+                    port = URLProtocol.HTTPS.defaultPort
+                    host = BASE_API
+                    path("movie", movieId)
+                    parameter("api_key", APIKEY)
                 }
             }
 
-            callback(result)
+            callback(JSON.parse(result))
         }
     }
 }
